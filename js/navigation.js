ac610bb1eb5d51c85665cac6c84ccfdfb44ea11f
alojzije <shljunak@gmail.com>
2014-09-18 13:20:46 +0200

    Bind click event to tretmani and wellness dan links instead of just thwir respective arrows

--- js/navigation.js
+++ js/navigation.js
@@ -1,8 +1,8 @@
-var $w   = $(window);
-var $arrow1 = $('.arrow');
-	jQuery.extend($arrow1, { isBound : false, subMenu : $('.subMenu') });
-var $arrow2 = $('.arrow2');
-	jQuery.extend($arrow2, { isBound : false, subMenu : $('.subMenu2') });
+var $w = $(window);
+var $subLink1 = $('.linkTretmani');
+	jQuery.extend($subLink1, { isBound : false, arrow : $('.arrow'), subMenu : $('.subMenu') });
+var $subLink2 = $('.linkWellness');
+	jQuery.extend($subLink2, { isBound : false, arrow : $('.arrow2'), subMenu : $('.subMenu2') });
 var $nav = $('.navigationLinks');
 	jQuery.extend($nav, { isFixed : false, homeY : $nav.offset().top });
 
@@ -12,19 +12,19 @@ function isMobileWidth(){
     return $(window).width() < 750;
 }
 
-function bindArrowClickEvent($arrow){
-	$arrow.isBound = true;
-	$arrow.click(function() {
-		$arrow.toggleClass('transitionArrow');
-		$arrow.subMenu.toggleClass('transitionSubMenu');
+function bindSubMenuClickEvent($subLink){
+	$subLink.isBound = true;
+	$subLink.click(function() {
+		$subLink.arrow.toggleClass('transitionArrow');
+		$subLink.subMenu.toggleClass('transitionSubMenu');
 	});
 }
 
-function unbindArrowClickEvent($arrow){
-	$arrow.isBound = false;
-	$arrow.unbind('click');
-	$arrow.removeClass('transitionArrow');
-	$arrow.subMenu.removeClass('transitionSubMenu');
+function unbindSubMenuClickEvent($subLink){
+	$subLink.isBound = false;
+	$subLink.unbind('click');
+	$subLink.arrow.removeClass('transitionArrow');
+	$subLink.subMenu.removeClass('transitionSubMenu');
 }
 
 function checkShouldNavFloat(){
@@ -34,15 +34,15 @@ function checkShouldNavFloat(){
 			$nav.css({position : 'fixed', top : 0});
 			$nav.isFixed = true;
 			if(!isMobileWidth()){
-				$arrow1.subMenu.css({position : 'fixed'});
-				$arrow2.subMenu.css({position : 'fixed'});
+				$subLink1.subMenu.css({position : 'fixed'});
+				$subLink2.subMenu.css({position : 'fixed'});
 			}
 		}
 		else if (!shouldBeFixed && $nav.isFixed ){
 			$nav.css({position : ''});
 			$nav.isFixed = false;
-			$arrow1.subMenu.css({position : ''});
-			$arrow2.subMenu.css({position : ''});
+			$subLink1.subMenu.css({position : ''});
+			$subLink2.subMenu.css({position : ''});
 		}
 }
 
@@ -53,9 +53,9 @@ $(document).ready(function(){
 	});
 	
 	// animate submenues and arrow for mobile-width window
-	if (isMobileWidth() && !$arrow1.isBound){
-		bindArrowClickEvent($arrow1);
-		bindArrowClickEvent($arrow2);
+	if (isMobileWidth() && !$subLink1.isBound){
+		bindSubMenuClickEvent($subLink1);
+		bindSubMenuClickEvent($subLink2);
 	}
 
 	//float the navigation on top upon scroll
@@ -66,20 +66,20 @@ $(document).ready(function(){
 $(window).resize(function(){
 	if (isMobileWidth()){
 		//fix the submenu navigation if needed
-		$arrow1.subMenu.css({position : ''});
-		$arrow2.subMenu.css({position : ''});
-		if (!$arrow1.isBound ){
-			bindArrowClickEvent($arrow1);
-			bindArrowClickEvent($arrow2);
+		$subLink1.subMenu.css({position : ''});
+		$subLink2.subMenu.css({position : ''});
+		if (!$subLink1.isBound ){
+			bindSubMenuClickEvent($subLink1);
+			bindSubMenuClickEvent($subLink2);
 		}
 	}else{
 		if($w.scrollTop() > $nav.homeY ){
-			$arrow1.subMenu.css({position : 'fixed'});
-			$arrow2.subMenu.css({position : 'fixed'});
+			$subLink1.subMenu.css({position : 'fixed'});
+			$subLink2.subMenu.css({position : 'fixed'});
 		}
-		if ( $arrow1.isBound ){
-			unbindArrowClickEvent($arrow1);
-			unbindArrowClickEvent($arrow2);
+		if ( $subLink1.isBound ){
+			unbindSubMenuClickEvent($subLink1);
+			unbindSubMenuClickEvent($subLink2);
 		}
 	}
 
