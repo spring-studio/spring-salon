a582ff8f4fa2e695fcd95c7d0fa4b9d92ad4e969
alojzije <shljunak@hotmail.com>
2014-11-05 14:24:31 +0100

    Make the mail.js into a biding function and add some css to response message

--- js/mail.js
+++ js/mail.js
@@ -1,29 +1,40 @@
-$(function(){
-	var form_desktop = $('#ajax-contact-desktop');
-
-	var formMessages =$('#form-messages');
+function bindMailToForm(form_id, responseMsg_id){
+	var form_desktop = $(form_id);
+	var responseMsg = $(responseMsg_id);
 
 	form_desktop.submit(function(e){
 		e.preventDefault();
 		var formData = form_desktop.serialize();
+		responseMsg.hide()
+			.attr('class', '')
+           .addClass('response-waiting')
+           .fadeIn(200);
+
 		$.ajax({
 		    type: "POST",
 		    url: form_desktop.attr('action'),
 		    data: formData
 		}).done(function(response) {
-		    // Make sure that the formMessages div has the 'success' class.
+		    // Make sure that the responseMsg div has the 'success' class.
 		    // Set the message text.
-		    formMessages.text('Hvala Vam. Uskoro ce Vas netko kontaktirati');
+		    responseMsg.hide()
+		   	 .attr('class', '')
+		     .addClass('response-success')
+		    .text('Vaša poruka je poslana. Hvala Vam')
+		    .fadeIn(2000);
 		    // Clear the form.
 			form_desktop[0].reset();
 		}).fail(function(data) {
-		        formMessages.text('Oops! An error occured and your message could not be sent.');
+		        responseMsg.hide()
+		        .attr('class', '')
+		     	.addClass('response-error')
+		        .text('Dogodila se greška, poruka nije poslana. Molimo Vas, pokušajte ponovno.')
+		        .fadeIn(2000);
 		    
 		});
 	});
+}
 
-
-
-});
+bindMailToForm('#responsive_contact', '#responseMsg');
 
 
